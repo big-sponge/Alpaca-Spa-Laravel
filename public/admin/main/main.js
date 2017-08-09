2ce93ef6e7240d5fbd02a1b9c067d103a945514a
cheng <cheng@cheng.com>
2017-08-09 11:19:12 +0800

    添加跨域参数

--- public/admin/main/main.js
+++ public/admin/main/main.js
@@ -135,6 +135,10 @@ AlpacaAjax = function (param) {
         url: g_url + "",
         data: {},
         dataType: "json",
+        xhrFields: {
+            withCredentials: true
+        },
+        crossDomain: true,
         async: true,
         beforeSend: function () {
             var block = $('body');
